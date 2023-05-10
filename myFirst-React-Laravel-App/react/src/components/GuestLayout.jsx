de8bf4b3a6898c3ad2700b1929a9c67411a10adb
daoistcoder <deservekindness@gmail.com>
2023-05-10 17:45:44 +0800

    add context folder & ContextProvider.jsx, update router.jsx & Default Layout.jsx

--- myFirst-React-Laravel-App/react/src/components/GuestLayout.jsx
+++ myFirst-React-Laravel-App/react/src/components/GuestLayout.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 export default function GuestLayout() {
     
     return (
-        <>
-        
-        </>
+       <div>
+            For guest users only
+            <Outlet />
+       </div>
     )
 }
--- myFirst-React-Laravel-App/react/src/router.jsx
+++ myFirst-React-Laravel-App/react/src/router.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {Navigate, createBrowserRouter} from "react-router-dom";
 import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Users from "./views/Users.jsx";
@@ -13,6 +13,10 @@ const router = createBrowserRouter( [
         path: '/',
         element: <DefaultLayout />,
         children:[
+            {
+                path: '/',
+                element: <Navigate to="/users" />
+            },
             {
                 path: '/dashboard',
                 element: <Dashboard />
