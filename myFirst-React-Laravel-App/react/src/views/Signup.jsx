5ea0354d660703f83ce8b8fdcddb54e84a847668
daoistcoder <deservekindness@gmail.com>
2023-05-10 22:25:25 +0800

    update Signup.jsx

--- myFirst-React-Laravel-App/react/src/views/Signup.jsx
+++ myFirst-React-Laravel-App/react/src/views/Signup.jsx
@@ -1,5 +1,7 @@
-import { useRef } from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import axiosClient from "../axios-client.js";
+import {useStateContext, setUser, setToken} from "../context/ContextProvider.jsx"
 
 export default function Signup() {
     const nameRef = useRef();
@@ -7,14 +9,27 @@ export default function Signup() {
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
 
+    const {setUser, setToken} = useStateContext();
+
     const onSubmit = (ev) => {
         ev.preventDefault()
         const payload = {
             name: nameRef.current.value,
             email: emailRef.current.value,
             password: passwordRef.current.value,
-            passwordConfirmation: passwordConfirmationRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
         }
+        axiosClient.post('/signup', payload)
+            .then(({data}) => {
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catach (err => {
+                const response = err.response;
+                if (response && response.status == 422) {
+                    console.log(response.data.errors);
+                }
+            })
     }
 
     return (
