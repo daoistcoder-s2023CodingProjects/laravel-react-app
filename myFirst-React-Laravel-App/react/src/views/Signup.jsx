c3a1a26ed55f7eaae115bbb90e1ef7969b0d9505
daoistcoder <deservekindness@gmail.com>
2023-05-10 20:20:35 +0800

    update Signup.jsx w/ refs

--- myFirst-React-Laravel-App/react/src/views/Signup.jsx
+++ myFirst-React-Laravel-App/react/src/views/Signup.jsx
@@ -1,9 +1,20 @@
+import { useRef } from "react"
 import { Link } from "react-router-dom"
 
 export default function Signup() {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
 
     const onSubmit = (ev) => {
         ev.preventDefault()
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            passwordConfirmation: passwordConfirmationRef.current.value,
+        }
     }
 
     return (
@@ -13,13 +24,13 @@ export default function Signup() {
                     <h1 className="title">
                         Signup for free
                     </h1>
-                    <input type="fullname" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Password Confirmation" />
+                    <input ref={nameRef} placeholder="Full Name" />
+                    <input ref={emailRef} type="email" placeholder="Email Address" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
+                    <input ref={passwordConfirmationRef} type="password" placeholder="Password Confirmation" />
                     <button className="btn btn-block">Signup</button>
                     <p className="message">
-                        Already Registered <Link to="/login">Sign</Link>
+                        Already Registered? <Link to="/login">Sign in</Link>
                     </p>
                 </form>
             </div>
