9b60b3541590445afad240a9250a37b93f81640a
daoistcoder <deservekindness@gmail.com>
2023-05-10 22:54:33 +0800

    update AuthController.php for public function login

--- myFirst-React-Laravel-App/routes/api.php
+++ myFirst-React-Laravel-App/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,3 +29,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //         'status_code' => 200
 //     ]);
 // });
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
