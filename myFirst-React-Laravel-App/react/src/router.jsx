a7704fbb2340142584de893f3f39d5fe0b59e68d
daoistcoder <deservekindness@gmail.com>
2023-05-13 12:28:54 +0800

    fixed DefaultLayout filename & callbacks

--- myFirst-React-Laravel-App/react/src/router.jsx
+++ myFirst-React-Laravel-App/react/src/router.jsx
@@ -3,9 +3,9 @@ import Login from "./views/Login.jsx";
 import Signup from "./views/Signup.jsx";
 import Users from "./views/Users.jsx";
 import NotFound from "./views/NotFound.jsx";
-import DefaultLayout from "./components/DefeaultLayout.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
 import GuestLayout from "./components/GuestLayout.jsx";
-import Dashboard from "./views/DashBoard.jsx";
+import Dashboard from "./views/Dashboard.jsx";
 
 
 const router = createBrowserRouter( [
