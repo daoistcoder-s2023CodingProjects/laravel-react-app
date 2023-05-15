2321fab801a343fa7d379485d86c49a5e7dfcabd
daoistcoder <deservekindness@gmail.com>
2023-05-15 16:49:13 +0800

    Update Users.jsx component
    now it can show the fetched data from tables

--- myFirst-React-Laravel-App/react/src/views/Users.jsx
+++ myFirst-React-Laravel-App/react/src/views/Users.jsx
@@ -20,6 +20,7 @@ export default function Users() {
             .then(({data}) => {
                 setLoading(false)
                 console.log(data);
+                setUsers(data.data);
             })
             .catch(() => {
                 setLoading(false)
@@ -32,6 +33,32 @@ export default function Users() {
             <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                 <h1>Users</h1>
                 <Link to="/users/new" className="btn-add">Add new</Link>           
+            </div>
+            <div className="card animated fadeInDown">
+                <table>
+                    <thead>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Create Date</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        {users.map(u => (
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.email}</td>
+                                <td>{u.created_at}</td>
+                                <td>
+                                    <Link className="btn-edit" to={'/users/'+u.id}>Edit</Link>
+                                    &nbsp;
+                                    <button className="btn-delete">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
         )
