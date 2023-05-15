bebb6cc355640f674ba491ca08d2a9bc69512ca9
daoistcoder <deservekindness@gmail.com>
2023-05-15 16:02:19 +0800

    Update UpdateUserRequest.php
    add rules to validate name, email, password

--- myFirst-React-Laravel-App/app/Http/Requests/StoreUserRequest.php
+++ myFirst-React-Laravel-App/app/Http/Requests/StoreUserRequest.php
@@ -5,7 +5,6 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 
-
 class StoreUserRequest extends FormRequest
 {
     /**
@@ -25,7 +24,7 @@ class StoreUserRequest extends FormRequest
     {
         return [
             'name' => ['required','string','max:55'],
-            'email' => ['required','email','unique:users','email'],
+            'email' => ['required','email','unique:users, email'],
             'password' => [
                 'required',
                 'confirmed',
